cebf9c5fd8930ea3a07e27de75e6dc1572a8994c
douge <1041790491@qq.com>
2023-08-11 16:59:02 +0800

    神经网络练习

---
+++ python神经网络编程学习/neural_network.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class NeuralNetwork:
+
+    def __init__(self, inodes, hnodes, onodes, lr) -> None:
+        self.inodes = inodes
+        self.hnodes = hnodes
+        self.onodes = onodes
+
+        self.lr = lr
+        pass
+
+
+    def learn(self):
+        pass
+
+
+    def query(self):
+        pass
+
